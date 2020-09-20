12f1bda7e5e9cc280204aa6b24b030e2b9f880a3
subroh0508 <subroh.0508@gmail.com>
2020-09-20 11:23:31 +0900

    Initial Commit

---
+++ helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by にしこりさぶろ〜 on 2020/09/20.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
